6cf1179860cc4168535e41c726e5f3c4cc1cf256
William Skou Heidemann <Williamskou2227@gmail.com>
2023-05-14 08:57:17 +1000

    4 NEW LEVELS

--- Assets/Scripts/TileComponents/TeleportTile.cs
+++ Assets/Scripts/TileComponents/TeleportTile.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class TeleportTile : TileComponentBase, IActivatedTile
 {
@@ -6,5 +7,10 @@ public class TeleportTile : TileComponentBase, IActivatedTile
     public void Activate()
     {
         PlayerMovement.Instance.MoveRequest(new MoveCommand(connectedTile, shouldTriggerTileEvent: false, shouldActivateTile: false, shouldIncrementStepCount: false, shouldCheckRange:false, shouldDisplayAllTiles: true));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, connectedTile.transform.position, Color.black);
     }
 }
