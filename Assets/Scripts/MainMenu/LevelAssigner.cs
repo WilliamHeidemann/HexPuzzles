0f90b6719cea9400151f03872035476eeaf74f18
William Skou Heidemann <Williamskou2227@gmail.com>
2023-05-20 13:49:01 +1000

    each level shows none, silver or gold star. Refactor code later

--- Assets/Scripts/MainMenu/LevelAssigner.cs
+++ Assets/Scripts/MainMenu/LevelAssigner.cs
@@ -9,6 +9,9 @@ public class LevelAssigner : MonoBehaviour
     [SerializeField] private World world;
     [SerializeField] private LevelButton centerButton;
     [SerializeField] private LevelButton[] outerButtons;
+    [SerializeField] private Sprite goldStar;
+    [SerializeField] private Sprite silverStar;
+    
     private void Start() => AssignLevels();
     private void AssignLevels()
     {
@@ -17,6 +20,11 @@ public class LevelAssigner : MonoBehaviour
         {
             centerButton.levelAssignedToButton = world.centerLevel;
             centerButton.world = world;
+            if (PlayerPrefs.GetInt(world.centerLevel.name) > 0) centerButton.transform.GetChild(0).GetComponent<Image>().color = Color.white;
+            else centerButton.transform.GetChild(0).GetComponent<Image>().color = Color.black;
+
+            centerButton.transform.GetChild(0).GetComponent<Image>().sprite =
+                PlayerPrefs.GetInt(world.centerLevel.name) == 3 ? goldStar : silverStar;
         }
         
         for (int i = 0; i < 6; i++)
@@ -26,6 +34,12 @@ public class LevelAssigner : MonoBehaviour
             {
                 outerButtons[i].levelAssignedToButton = world.connectedLevels[i];
                 outerButtons[i].world = world;
+                
+                if (PlayerPrefs.GetInt(world.connectedLevels[i].name) > 0) outerButtons[i].transform.GetChild(0).GetComponent<Image>().color = Color.white;
+                else outerButtons[i].transform.GetChild(0).GetComponent<Image>().color = Color.black;
+
+                outerButtons[i].transform.GetChild(0).GetComponent<Image>().sprite =
+                    PlayerPrefs.GetInt(world.connectedLevels[i].name) == 3 ? goldStar : silverStar;
             }
         }
     }
